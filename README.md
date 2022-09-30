22ee9def3233fc0bfb70657fbbf523bc63999bfd
ckd1618 <ckd1618@gmail.com>
2022-09-30 03:58:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# percentageGame
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-62aqvq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  scrollCount = 0;
+  bonusAtk = 0;
+  bonusDex = 0;
+  itemDestroyed = false;
+  x10sUsed = 0;
+  x30sUsed = 0;
+  itemCount = 1;
+
+  constructor() {
+
+  }
+
+  random(successRate,atk, dex, explodePercent) {
+    var val = Math.ceil(Math.random()*100);
+    var percent = 100-successRate;
+    var explode = percent * explodePercent / 100;
+    console.log(val, percent, explode)
+    if (val > percent) {
+      this.success(atk, dex);
+    } else if (explodePercent !== 0 && val <= explode) {
+      this.explodeItem();
+    } else {
+      this.fail();
+    }
+  }
+
+  try10() {
+    var atk = 5;
+    var dex = 3;
+    var explodePercent = 0;
+    var successRate = 10;
+    this.x10sUsed++;
+    this.random(successRate, atk, dex, explodePercent)
+  }
+  
+  try30() {
+    var atk = 5;
+    var dex = 3;
+    var explodePercent = 50;
+    var successRate = 30;
+    this.x30sUsed++;
+    this.random(successRate, atk, dex, explodePercent)
+  }
+
+  success(atk, dex) {
+    this.bonusAtk += atk;
+    this.bonusDex += dex;
+    this.scrollCount++;
+  }
+
+  fail() {
+    this.scrollCount++;
+  }
+
+  explodeItem() {
+    this.itemDestroyed = true;
+  }
+
+  clear() {
+    this.scrollCount = 0;
+    this.bonusAtk = 0;
+    this.bonusDex = 0;
+    this.itemDestroyed = false;
+    this.itemCount++;
+  }
+
+  resetAll() {
+    this.scrollCount = 0;
+    this.bonusAtk = 0;
+    this.bonusDex = 0;
+    this.itemDestroyed = false;
+    this.x10sUsed = 0;
+    this.x30sUsed = 0;
+    this.itemCount = 1;
+  }
+}
+
